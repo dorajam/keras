0dc30d982e6fc38801851c8e837379cb5f336111
Dora Jambor <dora.jambor@shopify.com>
2016-11-25 13:59:19 -0500

    adds classifiers to setup.py

--- setup.py
+++ setup.py
@@ -15,4 +15,11 @@ setup(name='Keras',
           'h5py': ['h5py'],
           'visualize': ['pydot-ng'],
       },
+      classifiers=[
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+      ]
       packages=find_packages())
